eabf2a61c61b8aa70ff6c37298cc974588e0db1e
Muhammad Sani Bello <muhammadsanibello0062@gmail.com>
2026-04-04 00:10:36 +0100

    Add LoginSimulator and EvenOddChecker projects with structured folders

---
+++ 02-Conditionals/EvenOddChecker/Program.cs
@@ -0,0 +1,21 @@
+﻿// EvenOddChecker
+
+Console.WriteLine("Enter a Number:\n");
+string? input = Console.ReadLine();
+int number = 0;
+
+if (int.TryParse(input, out number))
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine($"{number} is even");
+    }
+    else
+    {
+        Console.WriteLine($"{number} is odd");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid number.");
+}
---
+++ 02-Conditionals/LoginSimulator/Program.cs
@@ -0,0 +1,36 @@
+﻿// LoginSimulator
+string userName = "ByteGuardian19";
+string password = "63341234";
+int attempts = 0;
+int maxAttempts = 3;
+
+while (attempts < maxAttempts)
+{
+    Console.Write("Enter UserName:\t");
+    string? userNameResponse = Console.ReadLine();
+
+    Console.Write("Enter Password:\t");
+    string? passwordResponse = Console.ReadLine();
+
+    if (!string.IsNullOrEmpty(userNameResponse) && !string.IsNullOrEmpty(passwordResponse) && userNameResponse.Equals(userName, StringComparison.OrdinalIgnoreCase) && passwordResponse == password)
+    {
+        Console.WriteLine("\nLogin Successful");
+        break;
+    }
+
+    else
+    {
+        attempts++;
+        int remainingAttempts = maxAttempts - attempts;
+
+        if (remainingAttempts > 0)
+        {
+            Console.WriteLine($"\nInvalid Credentials! {remainingAttempts} attempt(s) left...\n");
+        }
+
+        else
+        {
+            Console.WriteLine("\nToo many attempts! System locked");
+        }
+    }
+}
