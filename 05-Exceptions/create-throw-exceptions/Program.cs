342a4bc462ef1deccf7064e3866ef76077104485
Muhammad Sani Bello <muhammadsanibello0062@gmail.com>
2026-04-19 01:55:24 +0100

    feat: extend exception handling with custom exceptions, throw statements, and advanced error handling

---
+++ 05-Exceptions/create-throw-exceptions/Program.cs
@@ -0,0 +1,42 @@
+﻿string[][] userEnteredValues = new string[][]
+{
+    new string[] {"1", "two", "3"},
+    new string[] {"0", "1", "2"}
+};
+
+foreach (string[] userEntries in userEnteredValues)
+{
+    try
+    {
+        BusinessProcess1(userEntries);
+    }
+    catch (Exception ex)
+    {
+        if (ex.StackTrace.Contains("BusinessProcess1") && (ex is FormatException))
+        {
+            Console.WriteLine("An error has occured");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
+static void BusinessProcess1(string[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        try
+        {
+            valueEntered = int.Parse(userValue);
+
+            // completes required calculations based on userValue
+            // ...
+        }
+        catch (FormatException)
+        {
+            FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
+            throw invalidFormatException;
+        }
+    }
+}
