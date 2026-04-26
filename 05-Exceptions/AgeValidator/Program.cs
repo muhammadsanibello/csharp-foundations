f21ba69e4ef75fff584f18e4dfba2daf5610de94
Muhammad Sani Bello <muhammadsanibello0062@gmail.com>
2026-04-26 23:18:20 +0100

    feat: Implement Age Validator program using exception and validation logic. refactor: Add exception handling logic to withdraw feature

---
+++ 05-Exceptions/AgeValidator/Program.cs
@@ -0,0 +1,35 @@
+﻿// Age Validator App
+
+// Prompt user for his age
+Console.Write("Enter your age: ");
+string? response = Console.ReadLine();
+
+if (int.TryParse(response, out int age))
+{
+    try
+    {
+        ValidRange();
+    }
+    catch (Exception ex)
+    {
+        Console.Write("Error! " + ex.Message);
+    }
+}
+else
+{
+    Console.WriteLine("Error! Age must be a number.");
+}
+
+// Range validity checking
+void ValidRange()
+{
+    if (age >= 1 && age <= 120)
+    {
+        Console.WriteLine("You are Welcome!");
+    }
+    else
+    {
+        throw new Exception("\"InvalidRange\": Age is out of range.");
+    }
+    Console.WriteLine();
+}
