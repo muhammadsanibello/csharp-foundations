ad4f584d0a53a6dfd3e8785258d032aabf71757e
Muhammad Sani Bello <muhammadsanibello0062@gmail.com>
2026-07-08 21:41:41 +0100

    feat: add file-based log persistence with FileSource and LogRepository

---
+++ Intermediate/07-Capstone-Projects/04-Logging-System/FileSource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LoggingSystem
+{
+    public static class FileSource
+    {
+        public static string GetFilePath()
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string directoryName = "LogData";
+            string directoryPath = Path.Combine(desktopPath, directoryName);
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            string fileName = "logs.json";
+            string filePath = Path.Combine(directoryPath, fileName);
+
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+            }
+
+            return filePath;
+        }
+     
+    }
+}
---
+++ Intermediate/07-Capstone-Projects/04-Logging-System/LogRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.Json;
+
+namespace LoggingSystem
+{
+    public class LogRepository
+    {
+        // Method of saving logs to a file
+        public async Task SaveLogsAsync(LogManager manager)
+        {
+            var logObjects = manager.logEntries();
+            string JsonString = JsonSerializer.Serialize(logObjects);
+
+            await File.WriteAllTextAsync(FileSource.GetFilePath(), JsonString);
+        }
+
+        // Method of loading logs from the file
+        public async Task<string> LoadLogsAsync()
+        {
+            var jsonString = await File.ReadAllTextAsync(FileSource.GetFilePath());
+            return jsonString;
+
+        }
+    }
+}
