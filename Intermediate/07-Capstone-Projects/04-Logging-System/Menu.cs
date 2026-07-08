9ca21aa02be0741c816e2292822f196f1447520c
Muhammad Sani Bello <muhammadsanibello0062@gmail.com>
2026-07-08 22:33:39 +0100

    feat: add user menus

---
+++ Intermediate/07-Capstone-Projects/04-Logging-System/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggingSystem
+{
+    public static class Menu
+    {
+        public static string GetUserMenu()
+        {
+            return "1. Create Log\n2. View Logs\n3. Search Log\n4. Delete Log\n5. Statistics\n6. Undo Delete\n7. Process Queue\n8. Save Logs\n0. Exit";
+        }
+    }
+}
