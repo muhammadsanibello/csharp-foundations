4740303a38e8dfe5b305a8199bd658ac3a859aab
Muhammad Sani Bello <muhammadsanibello0062@gmail.com>
2026-07-10 13:53:50 +0100

    feat: add helper for validating enum input

---
+++ Intermediate/07-Capstone-Projects/04-Logging-System/EnumInputHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LoggingSystem
+{
+    public static class EnumInputHelper
+    {
+        public static T GetEnumChoice<T>() where T : Enum
+        {
+            var values = (T[])Enum.GetValues(typeof(T));
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {values[i]}");
+            }
+
+            int choice = int.Parse(Console.ReadLine());
+
+            return values[choice - 1];
+        }
+    }
+}
