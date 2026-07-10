141eb608d2bf623ab91a77534cdec47d69aec50e
Muhammad Sani Bello <muhammadsanibello0062@gmail.com>
2026-07-10 14:02:27 +0100

    feat: implement log creation logic based on menu selection

--- Intermediate/07-Capstone-Projects/04-Logging-System/Program.cs
+++ Intermediate/07-Capstone-Projects/04-Logging-System/Program.cs
@@ -1,10 +1,37 @@
 ﻿using LoggingSystem;
 using System.Text.Json;
 
+var menu = Menu.GetUserMenu();
+string? input = string.Empty;
 var manager = new LogManager();
 
-manager.CreateLog(new LogEntry("12", "Database Connection Error", Level.Error, LogSource.Database, DateTime.Now));
-manager.CreateLog(new LogEntry("13", "Payment processing failed", Level.Error, LogSource.Payment, DateTime.Now));
+while (input != "0")
+{
+    Console.WriteLine(menu);
+    Console.WriteLine(Environment.NewLine);
+
+    Console.Write("Choose from the above options to continue: ");
+    input= Console.ReadLine();
+    MenuOptionValidation.ValidateOption(input);
+
+    switch (input)
+    {
+        case "1":
+
+            Console.Write("Enter Log ID: ");
+            string? id = Console.ReadLine();
+
+            Console.Write("Enter Log message: ");
+            string? message = Console.ReadLine();
+
+            Level level = EnumInputHelper.GetEnumChoice<Level>();
+            LogSource logSource = EnumInputHelper.GetEnumChoice<LogSource>();
+
+            manager.CreateLog(new LogEntry(id, message, level, logSource, DateTime.Now));
+
+            break;
+    }
+}
 
 var repository = new LogRepository();
 await repository.SaveLogsAsync(manager);
