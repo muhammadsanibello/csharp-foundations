1ddaa56fae81ca7f05361603ab1bc6fe3d8d259e
Muhammad Sani Bello <muhammadsanibello0062@gmail.com>
2026-07-11 13:01:58 +0100

    feat: add custom EventArgs class for critical event

---
+++ Intermediate/07-Capstone-Projects/04-Logging-System/CriticalEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LoggingSystem
+{
+    public class CriticalEventArgs : EventArgs
+    {
+        public LogEntry LogEntry {  get; private set; }
+
+        public CriticalEventArgs(LogEntry logEntry)
+        {
+            LogEntry = logEntry;
+        }
+    }
+}
