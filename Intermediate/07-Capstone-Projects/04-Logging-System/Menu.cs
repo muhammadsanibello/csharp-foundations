72e33b7b29c4b35b1a2be6ba5a8e1e6b04e98164
Muhammad Sani Bello <muhammadsanibello0062@gmail.com>
2026-07-11 15:23:31 +0100

    feat: add load option to user menu

--- Intermediate/07-Capstone-Projects/04-Logging-System/Menu.cs
+++ Intermediate/07-Capstone-Projects/04-Logging-System/Menu.cs
@@ -6,7 +6,7 @@ namespace LoggingSystem
     {
         public static string GetUserMenu()
         {
-            return "1. Create Log\n2. View Logs\n3. Search Log\n4. Delete Log\n5. Statistics\n6. Undo Delete\n7. Process Queue\n8. Save Logs\n0. Exit";
+            return "1. Create Log\n2. View Logs\n3. Search Log\n4. Delete Log\n5. Statistics\n6. Undo Delete\n7. Process Queue\n8. Save Logs\n9. Load Logs\n0. Exit";
         }
     }
 }
